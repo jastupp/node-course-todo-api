db04d38d0d6227ea698b305d1798628624bf3058
John Stupples <john.stupples@virginaustralia.com>
2019-01-29 06:57:13 +1000

    Added /todo/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,11 @@ const ObjectID = require('mongodb').ObjectID;
 const app  = require('../server').app;
 const Todo  = require('../models/todo').Todo;
 
-const test_data = [{text: 'First test todo'},
-                   {text: 'Second test todo'}];
+const id1 = '4c4ead502d8729705d9a4ccc';
+const id2 = '4c4ead502d8729705d9a4ccd';
+
+const test_data = [{_id: id1, text: 'First test todo'},
+                   {_id: id2, text: 'Second test todo'}];
 
 beforeEach((done) => {
    Todo.deleteMany({}).then(() => {
@@ -55,14 +58,41 @@ describe('POST /todos', () => {
     })
 });
 
-describe('GET /todos', (done) => {
-    it('Should return the test todos', () => {
+describe('GET /todos', () => {
+    it('Should return the test todos', (done) => {
         test(app)
             .get('/todos')
             .expect(200)
             .expect((response) => {
-                assert(response.body.length).toBe(2);
+                assert(response.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos:id', () => {
+    it('Should return a todo', (done) => {
+        test(app)
+            .get('/todos/' + id1)
+            .expect(200)
+            .expect((response) => {
+                //console.log('QQQQ', response.body.todo._id);
+                assert(response.body.todo._id).toBe(id1);
+            })
+            .end(done);
+    });
+
+    it('Should return a 404 as the id doest not exist', (done) => {
+        test(app)
+            .get('/todos/4c4ead502d8729705d9a4ccb')
+            .expect(404)
+            .end(done);
+    });
+
+    it('Sould return 404 with invalid id', (done) => {
+        test(app)
+            .get('/todos/' + id1 + 'q')
+            .expect(404)
             .end(done);
     });
 });
